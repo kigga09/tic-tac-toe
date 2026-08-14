c2a4f81962c3a7803d787c38b6b0008892b60518
khalil <khalilbelgh09@gmail.com>
2026-08-14 11:14:37 +0100

    the game is playable now, only thing left is the algorithm for the computer to play the best move

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-#----
+import random as rn
+
 real_moves = {
     '1': '',
     '2': '',
@@ -11,11 +12,28 @@ real_moves = {
     '9': ''
 }
 
-diagonals = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9'], ['1', '5', '9'], ['3', '5', '7'], ['1', '4', '7'], ['2', '5', '8'], ['3', '6', '9']]
+diagonals = [
+            ['1', '2', '3'],
+            ['4', '5', '6'],
+            ['7', '8', '9'],
+            ['1', '5', '9'],
+            ['3', '5', '7'],
+            ['1', '4', '7'],
+            ['2', '5', '8'],
+            ['3', '6', '9']
+            ]
 
-
-#functions
 moves = real_moves.keys()
+
+def result():
+    for i in diagonals:
+        if all_equal([real_moves[j] for j in i]) and real_moves[i[0]] != '':
+            return 'win'
+
+    if all(real_moves[i] != '' for i in moves):
+        return 'draw'
+
+    return None
 
 def txt_init(txt):
     txt = txt.lower()
@@ -23,10 +41,9 @@ def txt_init(txt):
     txt = txt.replace('\n', '')
     txt = txt.replace('\t', '')
     txt = txt.replace('\r', '')
-
+    return txt
 
 def isvalid(move):
-    global real_moves
     if move in moves:
         if real_moves[move] == '':
             return True
@@ -35,71 +52,111 @@ def isvalid(move):
     else:
         return False
 
-
 def all_equal(list):
     ref = list[0]
+
     for item in list:
         if item != ref:
             return False
-    return True
 
+    return True
 
 def show_table():
     print(f'''
-{real_moves['1']} - {real_moves['2']} - {real_moves['3']}
-{real_moves['4']} - {real_moves['5']} - {real_moves['6']}
-{real_moves['7']} - {real_moves['8']} - {real_moves['9']}
+{real_moves['1'] or '1'} - {real_moves['2'] or '2'} - {real_moves['3'] or '3'}
+{real_moves['4'] or '4'} - {real_moves['5'] or '5'} - {real_moves['6'] or '6'}
+{real_moves['7'] or '7'} - {real_moves['8'] or '8'} - {real_moves['9'] or '9'}
 ''')
 
-
 def player_turn(x_o):
-    global turn, move, moves, real_moves, running
-    #move = txt_init(move)
+    global move, running
+
+    move = txt_init(move)
+
     if move == 'q':
         running = False
-    elif move in moves:
-        for i in moves:
-            if move == i:
-                if isvalid(i):
-                    real_moves[i] = x_o
-                else:
-                    print('invalid move')
+        return False
+
+    elif isvalid(move):
+        real_moves[move] = x_o
+        return True
+
     else:
         print('invalid move')
+        return False
 
-
-#game loop
 
 choice = None
-turn = None #true--> players turn ; false--> computers turn
+turn = None
 running = True
+
 while running:
+
     if choice is None:
         choice = input('''chose who to make the 1st move
         0. you
         1. computer
-        q. quite 
+        q. quite
             (type 'q' whenever u wanna quit)
         > ''')
-        #choice = txt.init(choice)
+
+        choice = txt_init(choice)
+
         if choice == 'q':
             break
-        elif choice == '1':
-            turn = True
-            x_o = 'X'
+
         elif choice == '0':
+            turn = True
+            player_x_o = 'X'
+            computer_x_o = 'O'
+
+        elif choice == '1':
             turn = False
-            x_o = 'O'
+            player_x_o = 'O'
+            computer_x_o = 'X'
+
         else:
             print('invalid input')
+            choice = None
             continue
 
     if turn:
-        print('='*20)
+        print('=' * 20)
         show_table()
+
         move = input('whats ur move > ')
-        player_turn(x_o)
-        turn = False
+
+        if player_turn(player_x_o):
+            if result() == 'win':
+                show_table()
+                print('you win!')
+                break
+
+            elif result() == 'draw':
+                show_table()
+                print('draw!')
+                break
+
+            turn = False
+
     else:
-        print('='*20, '\ncomputer played the mbappe special')
+        print('=' * 20)
+
+        available = [i for i in moves if real_moves[i] == '']
+        move = rn.choice(available)
+
+        real_moves[move] = computer_x_o
+
+        print('computer played:', move)
+
+        if result() == 'win':
+            show_table()
+            print('computer wins!')
+            break
+
+        elif result() == 'draw':
+            show_table()
+            print('draw!')
+            break
+
         turn = True
